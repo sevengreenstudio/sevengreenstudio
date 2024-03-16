6570b11171c1c6421b00e926d392a9f6270b67f6
sevengreenstudio <sevengreenstudio@gmail.com>
2024-03-16 20:18:27 +0530

    1.0.0

--- login/js/database_js.js
+++ login/js/database_js.js
@@ -14,6 +14,16 @@ function database() {
             btn_login.innerHTML = "INVALID User or password";
         }
     }
+    //SevenGreenID
+    if (sevengreenid == "4950142745") {
+        if (username == "deepanraj" && password == "deepanraj2005") {
+            btn_login.innerHTML = "Loading...";
+            window.location = "main.html";
+        }
+        else {
+            btn_login.innerHTML = "INVALID User or password";
+        }
+    }
     //Else Part
     else {
         btn_login.innerHTML = "INVALID User or password";
--- login/js/sevengreenid.js
+++ login/js/sevengreenid.js
@@ -1,2 +1,3 @@
 let sevengreenID = [];
 sevengreenID[0] = "9985670779";
+sevengreenID[1] = "4950142745";
