ffc5aad3ee7d1447116613e1cd4ccf4e870660ee
sevengreenstudio <sevengreenstudio@gmail.com>
2024-03-17 12:45:53 +0000

    1.0.0

--- login/js/database_js.js
+++ login/js/database_js.js
@@ -3,29 +3,30 @@ function database() {
     const sevengreenid = login_sevengreenid.value;
     const username = login_username.value;
     const password = login_password.value;
-    
+
     //SevenGreenID
-    if (sevengreenid == "9985670779") {
+    if (sevengreenid == "6385776502") {
         if (username == "ajaysaagar" && password == "01032022") {
             btn_login.innerHTML = "Loading...";
             window.location = "main.html";
         }
         else {
             btn_login.innerHTML = "INVALID User or password";
+
         }
-    }
-    //SevenGreenID
-    if (sevengreenid == "4950142745") {
-        if (username == "deepanraj" && password == "deepanraj2005") {
-            btn_login.innerHTML = "Loading...";
-            window.location = "main.html";
+        //SevenGreenID
+        if (sevengreenid == "4950142745") {
+            if (username == "deepanraj" && password == "deepanraj2005") {
+                btn_login.innerHTML = "Loading...";
+                window.location = "main.html";
+            }
+            else {
+                btn_login.innerHTML = "INVALID User or password";
+            }
         }
+        //Else Part
         else {
             btn_login.innerHTML = "INVALID User or password";
         }
-    }
-    //Else Part
-    else {
-        btn_login.innerHTML = "INVALID User or password";
     }
 }
